8a287600aeaaed738273f633e61bd242fbebffae
yuita-yoshihiko <soup998211@gmail.com>
2025-07-19 18:09:22 +0900

    corsの設定

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import { Hono } from 'hono'
 import todos from './todos'
+import { cors } from 'hono/cors';
 
 const app = new Hono()
+
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:3000",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    maxAge: 86400,
+    credentials: true,
+  }),
+)
 
 app.route("/api/todos", todos);
 
